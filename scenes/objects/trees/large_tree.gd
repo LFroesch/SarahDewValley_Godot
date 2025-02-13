5d9230e3753a48afb1424c8d970e36d0c6d0ce8a
LFroesch <lucas.froeschner@gmail.com>
2025-02-13 09:01:27 -0800

    first commit

---
+++ scenes/objects/trees/large_tree.gd
@@ -0,0 +1,29 @@
+extends Sprite2D
+
+@onready var hurt_component: HurtComponent = $HurtComponent
+@onready var damage_component: Node2D = $DamageComponent
+
+var log_scene = preload("res://scenes/objects/trees/log.tscn")
+
+func _ready() -> void:
+	hurt_component.hurt.connect(on_hurt)
+	damage_component.max_damaged_reached.connect(on_max_damage_reached)
+	
+func on_hurt(hit_damage: int) -> void:
+	damage_component.apply_damage(hit_damage)
+	material.set_shader_parameter("shake_intensity", 1.0)
+	await get_tree().create_timer(1.0).timeout
+	material.set_shader_parameter("shake_intensity", 0.0)
+
+func on_max_damage_reached() -> void:
+	print("max damaged reached")
+	call_deferred("add_log_scene")
+	queue_free()
+	
+func add_log_scene() -> void:
+	var log_instance1 = log_scene.instantiate() as Node2D
+	log_instance1.global_position = global_position
+	get_parent().add_child(log_instance1)
+	var log_instance2 = log_scene.instantiate() as Node2D
+	log_instance2.global_position = global_position + Vector2(8,8)
+	get_parent().add_child(log_instance2)
---
+++ scenes/objects/trees/small_tree.gd
@@ -0,0 +1,28 @@
+extends Sprite2D
+
+@onready var hurt_component: HurtComponent = $HurtComponent
+@onready var damage_component: Node2D = $DamageComponent
+
+var log_scene = preload("res://scenes/objects/trees/log.tscn")
+
+func _ready() -> void:
+	hurt_component.hurt.connect(on_hurt)
+	damage_component.max_damaged_reached.connect(on_max_damage_reached)
+	
+func on_hurt(hit_damage: int) -> void:
+	damage_component.apply_damage(hit_damage)
+	material.set_shader_parameter("shake_intensity", 1.0)
+	#print("Current shader intensity: ", material.get_shader_parameter("shake_intensity"))
+	await get_tree().create_timer(1.0).timeout
+	material.set_shader_parameter("shake_intensity", 0.0)
+	#print("Current shader intensity: ", material.get_shader_parameter("shake_intensity"))
+
+func on_max_damage_reached() -> void:
+	print("max damaged reached")
+	call_deferred("add_log_scene")
+	queue_free()
+	
+func add_log_scene() -> void:
+	var log_instance = log_scene.instantiate() as Node2D
+	log_instance.global_position = global_position
+	get_parent().add_child(log_instance)
